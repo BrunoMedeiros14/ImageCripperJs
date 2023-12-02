9aefabf28257a1ca4afdc494237ef3f1931f7d7c
Bruno Medeiros <brunodemedeiros14@gmail.com>
2023-12-02 15:26:14 -0300

    feature: finish cropper function.

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 const imageButton = document.querySelector('#image-button');
 const imageInput = document.querySelector('#image-input');
-const previewDiv = document.querySelector('#preview-div');
+const previewDiv = document.querySelector('#cropCanva');
+const modal = document.querySelector('#modal');
+const removeCropBtn = document.querySelector('#removeCropBtn');
+const addCropBtn = document.querySelector('#addCropBtn');
+const previewDivImage = document.querySelector('#previewDivImage');
+let cropper;
+
+let prev = document.querySelectorAll('#previewCrop');
+
+const crop = image => {
+  return new Cropper(image, {
+    dragMode: 'move',
+    aspectRatio: 1,
+    viewMode: 1,
+    preview: prev,
+  });
+};
+
+const addUpdatedImage = src => {
+  const updatedImage = document.createElement('img');
+  const actualImage = document.querySelector('#updatedImage');
+
+  if (actualImage) {
+    actualImage.remove();
+  }
+
+  updatedImage.id = 'updatedImage';
+  updatedImage.width = 100;
+  updatedImage.height = 100;
+  updatedImage.src = src;
+  previewDivImage.appendChild(updatedImage);
+};
 
 imageButton.addEventListener('click', () => {
   imageInput.click();
@@ -8,6 +39,7 @@ imageButton.addEventListener('click', () => {
 
 imageInput.addEventListener('change', () => {
   const preview = document.querySelector('#preview-image');
+  const previewImage = document.createElement('img');
 
   if (preview) {
     preview.remove();
@@ -16,14 +48,29 @@ imageInput.addEventListener('change', () => {
   const reader = new FileReader();
 
   reader.onload = event => {
-    const previewImage = document.createElement('img');
-    previewImage.width = 115;
-    previewImage.height = 115;
     previewImage.id = 'preview-image';
-    previewImage.classList.add('pb-2');
+    previewImage.width = '100%';
     previewImage.src = event.target.result;
     previewDiv.appendChild(previewImage);
+    cropper = crop(previewImage);
+    modal.classList.remove('visually-hidden');
   };
 
   reader.readAsDataURL(imageInput.files[0]);
+});
+
+addCropBtn.addEventListener('click', event => {
+  if (cropper.cropped) {
+    cropper.getCroppedCanvas().toBlob(async blob => {
+      const objectURL = URL.createObjectURL(blob);
+      addUpdatedImage(objectURL);
+      cropper.destroy();
+      modal.classList.add('visually-hidden');
+    });
+  }
+});
+
+removeCropBtn.addEventListener('click', event => {
+  cropper.destroy();
+  modal.classList.add('visually-hidden');
 });
